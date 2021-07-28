b10182ce9d30e07776819a6808614cbf758ff703
RONENBE <RONENBE@rnd-hub.com>
2021-07-28 15:07:06 +0300

    Initial dashboard showing event list

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -1,10 +1,57 @@
 /** @jsxImportSource @emotion/react */
 import Page from '../layout/Page'
+import { useState, useRef, useEffect, useContext } from 'react'
+
+import { listEvents } from '../graphql/queries';
+import { API } from 'aws-amplify';
+import { CreateEvent as createEventMutation, DeleteEvent as deleteEventMutation } from '../graphql/mutations';
 
 const styles = {
   root: theme => ({}),
 }
 
-const Dashboard = () => <Page name="dashboard" css={styles.root}></Page>
+const initialFormState = { name: '', description: '' }
+
+const Dashboard = () => {
+
+  const [events, setEvents] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  async function fetchEvents() {
+    const apiData = await API.graphql({ query: listEvents });
+    setEvents(apiData.data.listEvents.items);
+  }
+
+  return (
+    <Page name="dashboard" css={styles.root}>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Event name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Event description"
+        value={formData.description}
+      />
+      <button>Create Event</button>
+      <div style={{marginBottom: 30}}>
+        {
+          events.map(event => (
+            <div key={event.id || event.name}>
+              <h2>{event.name}</h2>
+              <p>{event.description}</p>
+              <button>Delete event</button>
+            </div>
+          ))
+        }
+      </div>
+    </Page>
+  )
+}
 
 export default Dashboard
