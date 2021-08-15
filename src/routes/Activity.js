b4172c059cb28047fe5c5a5336c7ca02e7a52d98
RONENBE <RONENBE@rnd-hub.com>
2021-08-15 22:59:52 +0300

    created datastore utils

--- src/routes/Activity.js
+++ src/routes/Activity.js
@@ -3,7 +3,7 @@ import { useState, useRef, useEffect, useContext } from 'react'
 import useTheme from '../styling/useTheme'
 
 import Page from '../layout/Page'
-
+import { createNewReport, updateEndLocalTime } from '../utility/DatastoreUtils'
 import useTranslation from '../i18n/useTranslation'
 
 import IconButton from '@material-ui/core/IconButton'
@@ -16,8 +16,6 @@ import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add'
 
 import Walking from '@material-ui/icons/DirectionsWalk'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faHorse as Riding } from '@fortawesome/free-solid-svg-icons'
 import Riding from '@material-ui/icons/BedroomBaby'
 import Dining from '@material-ui/icons/Fastfood'
 import Sleeping from '@material-ui/icons/Hotel'
@@ -25,26 +23,14 @@ import Driving from '@material-ui/icons/DirectionsCarFilled'
 import WithFamily from '@material-ui/icons/FamilyRestroom'
 import Media from '@material-ui/icons/Theaters'
 import LayDown from '@material-ui/icons/AirlineSeatFlatAngled'
-// import ClosedSpace from '@material-ui/icons/ExitToApp'
 import ClosedSpace from '@material-ui/icons/DisabledByDefault'
 import Effort from '@material-ui/icons/FitnessCenter'
 import Crowded from '@material-ui/icons/Groups'
-// import lightBlue from '@material-ui/core/colors/lightBlue'
 import lightGreen from '@material-ui/core/colors/lightGreen'
 import red from '@material-ui/core/colors/red'
-// import TextField from '@material-ui/pickers';
-import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
-// import { API } from 'aws-amplify';
-// import { createEvent as createEventMutation, deleteEvent as deleteEventMutation } from '../graphql/mutations';
 
-import { DataStore, SortDirection } from '@aws-amplify/datastore';
-import { Event } from '../models';
 import { Auth } from 'aws-amplify';
-
-import moment from 'moment';
-
-DataStore.clear();
 
 
 const Section = ({ title, children, center = false }) => {
@@ -128,7 +114,6 @@ const useStyles = makeStyles((theme) => ({
 const initialFormState = { name: '', description: '' }
 
 const Activity = () => {
-  console.log("start")
   const t = useTranslation()
   const [majorEvent, setMajorEvent] = useState(false)
   const [majorEvents, setMajorEvents] = useState([]);
@@ -151,110 +136,35 @@ const Activity = () => {
   ]
   const [clickedActivities,setActivity] = useState([0]);
 
-  
-
-  //const [activity, setActivity] = useState()
   const [sentiment, setSentiment] = useState()
   const updateSentiment = clicked => async () => {
     if (clicked === sentiment) {
-      console.log(clicked)
-      const lastSentiment = await DataStore.query(Event, c =>
-        c.name("eq", clicked), 
-        {
-          sort: s => s.startLocalTime(SortDirection.DESCENDING),
-          limit: 1
-        }
-      );
-      await DataStore.save(
-        Event.copyOf(lastSentiment[0], updated => {
-          updated.endLocalTime = moment().format();
-      }));
-      DataStore.delete(lastSentiment[0]);
+      await updateEndLocalTime(clicked);
       setSentiment(null)
     } else if (sentiment != null) {
-      const lastSentiment = await DataStore.query(Event, c =>
-        c.name("eq", sentiment), 
-        {
-          sort: s => s.startLocalTime(SortDirection.DESCENDING),
-          limit: 1
-        }
-      );
-      await DataStore.save(
-        Event.copyOf(lastSentiment[0], updated => {
-          updated.endLocalTime = moment().format();
-      }));
-      DataStore.delete(lastSentiment[0]);
-      await DataStore.save(
-        new Event({
-          "name": clicked,
-          "startLocalTime": moment().format(),
-          "userName": userName
-        })
-      );  
+      await updateEndLocalTime(sentiment);
+      createNewReport(clicked, userName);
       setSentiment(clicked)
     } else {
-      console.log(clicked)
-      await DataStore.save(
-        new Event({
-          "name": clicked,
-          "startLocalTime": moment().format(),
-          "userName": userName
-        })
-      );
+      createNewReport(clicked, userName);
       setSentiment(clicked)
     }
   }
 
   const toggleMajorEvent = async () => {
     setMajorEvent(eventState => !eventState)
-    console.log("toggleMajorEvent")
     if (majorEvent){
-      const lastMajorEvent = await DataStore.query(Event, c =>
-        c.name("eq", "majorEvent"), 
-        {
-          sort: s => s.startLocalTime(SortDirection.DESCENDING),
-          limit: 1
-        }
-      );
-      await DataStore.save(
-        Event.copyOf(lastMajorEvent[0], updated => {
-          updated.endLocalTime = moment().format();
-      }));
-      DataStore.delete(lastMajorEvent[0]);
+      await updateEndLocalTime("majorEvent");
     } else {
-      await DataStore.save(
-        new Event({
-          "name": "majorEvent",
-          "startLocalTime": moment().format(),
-          "userName": userName
-        })
-      );
+      createNewReport("majorEvent", userName);
     }
   }
   const toggleModerateEvent = async () => {
     setModerateEvent(eventState => !eventState)
-    console.log("toggleModerateEvent")
     if (moderateEvent){
-      const lastModerateEvent = await DataStore.query(Event, c =>
-        c.name("eq", "moderateEvent"), 
-        {
-          sort: s => s.startLocalTime(SortDirection.DESCENDING),
-          limit: 1
-        }
-      );
-      await DataStore.save(
-        Event.copyOf(lastModerateEvent[0], updated => {
-          updated.endLocalTime = moment().format();
-      }));
-      DataStore.delete(lastModerateEvent[0]);
+      await updateEndLocalTime("moderateEvent");
     } else {
-      await DataStore.save(
-        new Event({
-          "name": "moderateEvent",
-          "startLocalTime": moment().format(),
-          "userName": userName
-        })
-      );
+      createNewReport("moderateEvent", userName);
     }
   }
 
@@ -271,44 +181,16 @@ const Activity = () => {
     { name: 'effort', icon: <Effort /> },
     { name: 'crowded', icon: <Crowded /> },
   ]
-  const getActivityByName = async (name) => {
-    const lastActivity = await DataStore.query(Event, c =>
-      c.name("eq", name), 
-      {
-        sort: s => s.startLocalTime(SortDirection.DESCENDING),
-        limit: 1
-      }
-    );
-    return lastActivity;
-  }
-  const updateEndLocalTime = async (Element) =>{
-    await DataStore.save(
-      Event.copyOf(Element, updated => {
-        updated.endLocalTime = moment().format();
-    }));
-  }
+
   const updateActivity = clicked => async () => {
     if (clickedActivities.includes(clicked)) {
       let index = clickedActivities.indexOf(clicked);
-
-      console.log(clicked)
-      const lastActivity = await getActivityByName(clicked);
-      await updateEndLocalTime(lastActivity[0]);
-      DataStore.delete(lastActivity[0]);
+      await updateEndLocalTime(clicked);
       setActivity(clickedActivities.filter(item=>item!==clicked));
-     
       clickedActivities.splice(index-1,1);
 
     }  else {
-      console.log(clicked)
-      await DataStore.save(
-        new Event({
-          "name": clicked,
-          "startLocalTime": moment().format(),
-          "userName": userName
-        })
-      );
-    
+      createNewReport(clicked, userName);
       setActivity(state => [...state,clicked]);
     }
   }
@@ -409,14 +291,6 @@ const Activity = () => {
       gap: '2rem',
     }
   }
-
-  // async function createEvent() {
-  //   console.log(formData)
-  //   if (!formData.name || !formData.description) return;
-  //   await API.graphql({ query: createEventMutation, variables: { input: formData } });
-  //   setEvents([ ...events, formData ]);
-  //   setFormData(initialFormState);
-  // }
 
   const classes = useStyles();
 
---
+++ src/utility/DatastoreUtils.js
@@ -0,0 +1,35 @@
+import { DataStore, SortDirection } from '@aws-amplify/datastore';
+import { Event } from '../models';
+import moment from 'moment';
+
+// DataStore.clear();
+
+const getActivityByName = async (name) => {
+    const lastActivity = await DataStore.query(Event, c =>
+      c.name("eq", name), 
+      {
+        sort: s => s.startLocalTime(SortDirection.DESCENDING),
+        limit: 1
+      }
+    );
+    return lastActivity;
+  }
+
+export const updateEndLocalTime = async (name) => {
+    const lastActivity = await getActivityByName(name);
+    await DataStore.save(
+        Event.copyOf(lastActivity[0], updated => {
+        updated.endLocalTime = moment().format();
+    }));
+    DataStore.delete(lastActivity[0]);
+}
+
+export const createNewReport = async (clicked, userName) => {
+    await DataStore.save(
+        new Event({
+        "name": clicked,
+        "startLocalTime": moment().format(),
+        "userName": userName
+        })
+    );
+}
